c240c6085f15d5d60c586ed4e17d8d61f43a827a
imadisimran <imadisimran@gmail.com>
2025-11-02 21:53:03 +0600

    footer and category

---
+++ src/components/LeftAside.jsx
@@ -0,0 +1,15 @@
+import React, { use } from 'react';
+import { NavLink } from 'react-router';
+const categoryPromise=fetch('/categoryData.json').then(response=>response.json())
+const LeftAside = () => {
+    const categoriesData = use(categoryPromise)
+    const categories = categoriesData.map(categoryData => <li key={categoryData.id}><NavLink className={({isActive})=>`w-full py-3 block rounded-xl ${isActive && 'font-bold bg-base-300'}`} to={`category/${categoryData.id}`}>{categoryData.name} {categoryData.emoji}</NavLink></li>)
+    return (
+
+        <ul className='space-y-5'>
+            {categories}
+        </ul>
+    );
+};
+
+export default LeftAside;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import LeftAside from '../components/LeftAside';
 
 const HomeLayout = () => {
     return (
@@ -7,6 +9,17 @@ const HomeLayout = () => {
             <header>
                 <Navbar></Navbar>
             </header>
+
+            <main className='grid-cols-12 grid w-11/12 mx-auto'>
+                <aside className='col-span-3'>
+                    <LeftAside></LeftAside>
+                </aside>
+                <section>
+                    
+                </section>
+            </main>
+
+            <Footer></Footer>
         </>
     );
 };
